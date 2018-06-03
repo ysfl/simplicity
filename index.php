cb18f5edb6c707a53ca9926f95b0517b51dc59cc
ysfl <ysfllxcn@live.com>
2018-06-03 14:52:49 +0800

    首页细节修改

--- index.php
+++ index.php
@@ -41,9 +41,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 				<div class="post-article">
 					<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
 					<div class="post-meta">
-						<span class="post-meta-dash"><?php _e(''); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></span>
-						<span class="post-meta-dash"><?php _e('In&nbsp;&nbsp;'); ?><?php $this->category(','); ?></span>
-						<span><?php get_post_view($this) ?><?php _e('&nbsp;&nbsp;views'); ?></span>
+						<span class="post-meta-dash"><?php _e('发布时间'); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></span>
+						<span class="post-meta-dash"><?php _e('分类'); ?><?php $this->category(','); ?></span>
+						<span><?php get_post_view($this) ?><?php _e('阅读量'); ?></span>
 					</div>
 					<?php if(isset($this->fields->post_cover)){  ?>
 					<div class="post-cover">
@@ -52,16 +52,16 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 					<?php };?>
 					<div class="post-content" itemprop="articleBody">
 		    			<a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->excerpt(300, '...'); ?></a>
-		            </div>
-		            <a href="<?php $this->permalink() ?>"><button class="btn btn-primary">Read On</button></a>
-		            <span class="post-comments" itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('<i class="iconfont icon-comment"></i>-', '<i class="iconfont icon-comment"></i>1', '<i class="iconfont icon-comment"></i>%d'); ?></a></span>
+		      </div>
+		            <a href="<?php $this->permalink() ?>"><button class="btn btn-primary">查看全文</button></a>
+		            <span class="post-comments" itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('<i class="iconfont icon-comment"></i>0', '<i class="iconfont icon-comment"></i>1', '<i class="iconfont icon-comment"></i>%d'); ?></a></span>
 				</div>
 			</div>
 		<?php endwhile; ?>
 			<div class="page-parameter col-12 col-md-12">
 				<h5><?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?><?php echo ' / ';?><?php echo ceil($this->getTotal() / $this->parameter->pageSize); ?></h5>
-				<?php $this->pageLink('<span class="btn btn-grey">next</span>', 'next') ?>
-	            <?php $this->pageLink('<span class="btn btn-grey">prev</span>', 'prev') ?>
+				<?php $this->pageLink('<span class="btn btn-grey">下一页</span>', 'next') ?>
+	            <?php $this->pageLink('<span class="btn btn-grey">上一页</span>', 'prev') ?>
 			</div>
 		</div>
 		</div>
